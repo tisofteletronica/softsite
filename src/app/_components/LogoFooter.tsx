2fa726d38a2ecde7c555487039fdd7d49ee534e4
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-16 18:29:12 -0300

    feat: add component LogoFooter

---
+++ src/app/_components/LogoFooter.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import imgLogo from "../../../public/logo-footer.png";
+
+interface LogoProps {
+  className?: string;
+}
+
+export function LogoFooter({ className }: LogoProps) {
+  return (
+    <Link href="/">
+      <Image src={imgLogo} alt="Logo Soft" className={className} />
+    </Link>
+  )
+}
