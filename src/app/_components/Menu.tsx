8929e2cd7f2661f1bff5ba8457a99afa2929ee68
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-16 18:29:31 -0300

    feat: add component Menu

--- src/app/_components/Menu.tsx
+++ src/app/_components/Menu.tsx
@@ -1,14 +1,28 @@
+'use client'
 import { MENU } from '@/config/constants';
+import { cn } from '@/lib/utils';
 import * as Menubar from '@radix-ui/react-menubar';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 import { GoChevronDown, GoChevronRight } from 'react-icons/go';
 
 export function Menu() {
+  const pathname = usePathname();
+
   return (
     <Menubar.Root className='flex flex-wrap flex-col lg:flex-row lg:gap-[50px] lg:mr-5'>
-      {MENU.map((menu, index) => (
-        <Menubar.Menu key={index}>
-          <Menubar.Trigger className="MenubarItem MenuItem" asChild>
+      {MENU.map((menu) => (
+        <Menubar.Menu key={menu.label}>
+          <Menubar.Trigger
+            className={
+              cn(
+                'MenubarItem MenuItem',
+                pathname === menu.href && 'menuActive'
+              )
+            }
+            asChild
+          >
             {menu.sub ? (
               <Link href={menu.href} className='flex items-center'>
                 {menu.label}
@@ -23,15 +37,13 @@ export function Menu() {
             )}
           </Menubar.Trigger>
 
-            {menu?.sub && (
+            {menu.sub && (
               <Menubar.Portal>
                 <Menubar.Content className="MenubarContent bg-white shadow-[0px_3px_6px_#00000029] flex flex-col py-4 lg:pt-8 lg:pb-4 mt-[-3px] z-[51] lg:z-[0]" align="center" sideOffset={5} alignOffset={-3}>
-                  {menu.sub?.map((submenu, index) => (
-                    <>
-
-
+                  {menu.sub?.map((submenu) => (
+                    <React.Fragment key={submenu.label}>
                       {submenu.sub ? (
-                        <Menubar.Sub>
+                        <Menubar.Sub key={submenu.label}>
                           <Menubar.SubTrigger className="MenubarItem MenubarItemSub MenubarSubTrigger">
                             <div className='flex items-center cursor-pointer'>
                               {submenu.label}
@@ -43,8 +55,8 @@ export function Menu() {
 
                           <Menubar.Portal>
                             <Menubar.SubContent className="MenubarSubContent bg-orange lg:bg-white shadow-[0px_3px_6px_#00000029] pt-4 pb-3 z-[51] lg:z-[0]">
-                              {submenu.sub?.map((subsub, index) => (
-                                <Menubar.Item key={index} className="MenubarItem MenubarItemSub MenubarItemSubSub">
+                              {submenu.sub?.map((subsub) => (
+                                <Menubar.Item key={subsub.label} className="MenubarItem MenubarItemSub MenubarItemSubSub">
                                   <Link href={subsub.href}>
                                     {subsub.label}
                                   </Link>
@@ -54,13 +66,13 @@ export function Menu() {
                           </Menubar.Portal>
                         </Menubar.Sub >
                       ) : (
-                          <Menubar.Item key={index} className="MenubarItem MenubarItemSub" asChild>
+                          <Menubar.Item key={submenu.label} className="MenubarItem MenubarItemSub" asChild>
                             <Link href={submenu.href}>
                               {submenu.label}
                             </Link>
                           </Menubar.Item>
                       )}
-                    </>
+                    </React.Fragment>
                   ))}
                 </Menubar.Content>
               </Menubar.Portal>
