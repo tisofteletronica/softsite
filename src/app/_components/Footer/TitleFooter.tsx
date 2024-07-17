3c81403b483210fbabb60fcef3bc58acb488f2bc
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-17 00:19:13 -0300

    feat: add component TitleFooter

---
+++ src/app/_components/Footer/TitleFooter.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+
+interface TitleFooterProps {
+  children: React.ReactNode;
+  className?: string;
+  type: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+}
+
+export function TitleFooter({ children, className, type }: TitleFooterProps) {
+  const Component = type;
+
+  return (
+    <Component
+      className={cn(
+        "text-white text-[24px] font-semibold",
+        className
+      )}
+    >
+      {children}
+      <hr className="border-0 m-0 w-10 h-[3px] bg-orange" />
+    </Component>
+  )
+}
