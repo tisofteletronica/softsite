8190d1543d6fd7e5d23d53973fba86e4e70e4b4a
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-19 19:44:02 -0300

    feat: add component Slider

---
+++ src/app/sobre-nos/_components/Slider.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { LineTimeResponse } from '@/services/aboutService/lineTime';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+export function Slider({ content }: LineTimeResponse) {
+  const pagination = {
+    clickable: true
+  };
+
+  return (
+    <div className='slider-about'>
+      <Swiper
+        pagination={pagination}
+        navigation
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {content.map((slide) => (
+          <SwiperSlide key={slide.id} className='px-3 lg:px-[180px] pt-4 pb-[31px] bg-[#F7F6FB] lg:bg-bgSlide lg:bg-transparent'>
+            <strong
+              className='text-[42px] font-semibold text-orange tracking-[1.7px] leading-[59px] italic mb-[9px]'
+            >
+              {slide.anos}
+            </strong>
+
+            <p
+              className='text-[18px] lg:text-[20px] font-medium leading-[23px] lg:leading-[30px] tracking-[2px] text-gray'
+            >
+              {slide.historico}
+            </p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
