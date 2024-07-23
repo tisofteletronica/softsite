1038dd2665cfe0a4fdb3ff7a032ef209384e4ee6
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-23 20:43:43 -0300

    feat: add component Form

---
+++ src/app/contato/_components/Form.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { Button } from "@/app/_components/Button";
+import { Input } from "@/app/_components/Input";
+import { Spinner } from "@/app/_components/Spinner";
+import { Textarea } from "@/app/_components/Textarea";
+import { useContactController } from "../useContactController";
+
+export function Form() {
+  const {
+    register,
+    errors,
+    handleSubmit,
+    isLoading
+  } = useContactController();
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <div className="grid lg:grid-cols-2 gap-4">
+        <Input
+          type="text"
+          placeholder="Nome*"
+          error={errors.nome?.message}
+          {...register('nome')}
+        />
+        <Input
+          type="email"
+          placeholder="E-mail*"
+          error={errors.email?.message}
+          {...register('email')}
+        />
+        <Input
+          type="text"
+          placeholder="Cidade*"
+          error={errors.cidade?.message}
+          {...register('cidade')}
+        />
+        <Input
+          type="text"
+          placeholder="País*"
+          error={errors.pais?.message}
+          {...register('pais')}
+        />
+        <Input
+          type="text"
+          placeholder="Empresa*"
+          error={errors.empresa?.message}
+          {...register('empresa')}
+        />
+        <Input
+          type="tel"
+          placeholder="Telefone*"
+          error={errors.telefone?.message}
+          {...register('telefone')}
+        />
+      </div>
+
+      <div className="mt-4">
+        <Input
+          type="text"
+          placeholder="Endereço*"
+          error={errors.endereco?.message}
+          {...register('endereco')}
+        />
+      </div>
+
+      <div className="mt-4">
+        <Textarea
+          placeholder="Mensagem*"
+          error={errors.mensagem?.message}
+          {...register('mensagem')}
+        />
+      </div>
+
+      <div className="mt-2.5">
+        <Button type="submit" className="px-8 font-normal uppercase">
+          Enviar
+          {isLoading && <Spinner className="w-4 h-4 m-0 ml-1.5" />}
+        </Button>
+      </div>
+    </form>
+  )
+}
