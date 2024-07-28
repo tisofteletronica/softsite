8547d286f5fcdf055eb70fd375ec47ceaabaffdb
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-28 17:46:43 -0300

    feat: add component CardSlider

---
+++ src/app/_components/CardSlider.tsx
@@ -0,0 +1,21 @@
+export interface CardSliderProps {
+  servicos: string;
+  descricao: string;
+}
+
+export function CardSlider({ servicos, descricao }: CardSliderProps) {
+  return (
+    <article className="w-full bg-white rounded-[0_40px] shadow-[0px_3px_6px_#00000029] min-h-[548px]">
+      <figure className="bg-[#F1F1FA] rounded-[0_40px] w-full h-[242px] overflow-hidden" />
+
+      <div className="w-full p-6 pb-[50px]">
+        <h3 className="uppercase text-gray font-bold text-lg leading-[22px] mb-5">
+          {servicos}
+        </h3>
+        <p className="text-gray text-sm leading-[19px] font-medium tracking-[1px]">
+          {descricao}
+        </p>
+      </div>
+    </article>
+  )
+}
