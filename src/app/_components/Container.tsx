1ee549aa756acd567f8755ebb31df602eec4bae8
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-28 17:46:00 -0300

    feat: add propertie id in component Container

--- src/app/_components/Container.tsx
+++ src/app/_components/Container.tsx
@@ -5,16 +5,17 @@ interface ContainerProps {
   children: React.ReactNode;
   className?: string;
   type: 'div' | 'section' | 'article' | 'main'
+  id?: string;
 }
 
-export function Container({ children, className, type }: ContainerProps) {
+export function Container({ children, className, type, id }: ContainerProps) {
   const Component = type;
 
   return (
     <Component className={cn(
       'w-full max-w-[1220px] m-auto px-3 lg:px-0',
       className
-    )}>
+    )} id={id}>
       {children}
     </Component>
   )
