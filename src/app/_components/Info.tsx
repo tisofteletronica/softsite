72b49938b62a374d2966ab6f2edca7ab801ea25f
Diego Ricardo <diegoricardoweb@gmail.com>
2024-07-30 23:22:06 -0300

    feat: add component Icon Info

---
+++ src/app/_components/Info.tsx
@@ -0,0 +1,7 @@
+export function Info() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="18" height="18" xmlSpace="preserve" version="1.1" viewBox="0 0 18 18">
+      <image width="18" height="18" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAABHNCSVQICAgIfAhkiAAAAQxJREFUOE+tlLENwkAMRck20NMDu9DAANBBoIIJKFiBHQg1NYJtwC86R451hkTiJCu6f/b3t32XYpBfU4EnYqU7Zn8Tq3xY4QAItmJ8rynonXzwBccg2llCS6TBOC3EXoHaoeCnRDhTMiWyJBx2WSgmriZTIkBWjuQo+ErMtwH/Jk7rBhgF5ZCVxtMTvyjziQCItLk5NUryEL9LUC8i6tKYShlkPAs+TwS50jhCSKlEbPZBxrXgh6BHhGwQ8XeiqDQy/lLUlPat2V2ImmYzmWj8dzkbm95VjNrsW+MHjy4kau3yD7Z1IXFUVT5jMMgazj4RT7YUgBubW18frQagTJuPOqz3b8Rmh7DXj+0DidJMjXUKIZAAAAAASUVORK5CYII=" />
+    </svg>
+  )
+}
