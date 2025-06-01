03cbf207170536154a0c2858d096d520461b064e
tisofteletronica <87986174+tisofteletronica@users.noreply.github.com>
2025-06-01 19:04:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/manual/:image',
+        destination: 'https://www.instalesoft.com.br/imagens/icons/:image',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+module.exports = nextConfig;
+
